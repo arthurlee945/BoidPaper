fb3ac32de5a57e0a21428416a47bb2b5141fe043
Arthur Lee <arthur.lee945@gmail.com>
2024-01-02 20:18:10 -0600

    feat: adding base package to start on BoidPaper

--- apps/next/src/app/page.tsx
+++ apps/next/src/app/page.tsx
@@ -1,10 +1,3 @@
-import { Button } from "@boidpaper/ui";
-
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello World
-      <Button>Sample button</Button>
-    </main>
-  );
+  return <main className="flex min-h-screen flex-col items-center justify-between p-24">Hello World</main>;
 }
