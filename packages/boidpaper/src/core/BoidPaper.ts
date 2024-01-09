a54d64a46fa72dabe8e8b892abf733d1ebfae260
Arthur Lee <arthur.lee945@gmail.com>
2024-01-08 20:23:00 -0600

    adding required abstract method placeholder

--- packages/boidpaper/src/core/BoidPaper.ts
+++ packages/boidpaper/src/core/BoidPaper.ts
@@ -1,3 +1,7 @@
 import { BaseBoidPaper } from "./extends/BaseBoidPaper";
 
-export class BoidPaper extends BaseBoidPaper {}
+export class BoidPaper extends BaseBoidPaper {
+  protected renderBoidMaterial(_delta: number, _cp: number): void {
+    throw new Error("Method not implemented.");
+  }
+}
--- packages/boidpaper/src/core/GLTFBoidPaper.ts
+++ packages/boidpaper/src/core/GLTFBoidPaper.ts
@@ -1,5 +1,7 @@
 import { BaseBoidPaper } from "./extends/BaseBoidPaper";
 
 export class GLTFBoidPaper extends BaseBoidPaper {
-    
+  protected renderBoidMaterial(_delta: number, _cp: number): void {
+    throw new Error("Method not implemented.");
+  }
 }
